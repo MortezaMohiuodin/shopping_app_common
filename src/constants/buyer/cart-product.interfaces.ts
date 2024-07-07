e7dc0d81df9f16fdfd071fffcae2a4973129de12
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-07-07 17:44:03 +0330

    delete product from cart & get cart routers

--- src/constants/buyer/cart-product.interfaces.ts
+++ src/constants/buyer/cart-product.interfaces.ts
@@ -3,7 +3,7 @@ import { CartDoc } from "./cart.interfaces";
 import { ProductDoc } from "../seller/product.interfaces";
 export interface CartProductDoc extends mongoose.Document{
     cart:CartDoc | string;
-    product : ProductDoc | string;
+    product : ProductDoc;
     quantity : number 
 }
 export interface CartProductModel extends mongoose.Model<CartProductDoc>{
